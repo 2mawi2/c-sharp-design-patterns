bdf5effb5714363e38d57b07da6641b0ad8c2d66
Marius Wichtner <mwichtner@coconut-capital.com>
2018-02-27 00:20:33 +0100

    Added Adapter Pattern

---
+++ DesignPatterns/Adapter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DesignPatterns
+{
+    interface IDestination
+    {
+        void BeautifulMethod();
+    }
+
+    public class Adapter : IDestination
+    {
+        private readonly IAdaptee _adaptee;
+
+        //adaptee should get injected, to allow testability
+        public Adapter(IAdaptee adaptee) => _adaptee = adaptee;
+
+        public void BeautifulMethod()
+        {
+            _adaptee.Authenticate();
+            _adaptee.FarTooLongSignatureOfMethodWhichShouldGetWrapped();
+        }
+    }
+
+    public interface IAdaptee
+    {
+        void FarTooLongSignatureOfMethodWhichShouldGetWrapped();
+        void Authenticate();
+    }
+
+    public class Adaptee : IAdaptee
+    {
+        public void Authenticate() => Console.WriteLine("authenticating");
+
+        public void FarTooLongSignatureOfMethodWhichShouldGetWrapped() => Console.WriteLine("doing sth");
+    }
+}
---
+++ DesignPatternsTest/AdapterTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.InteropServices.ComTypes;
+using DesignPatterns;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace DesignPatternsTest
+{
+    public class AdapterTest
+    {
+        [Fact]
+        public void CommandPatternTest()
+        {
+            var adaptee = new Mock<IAdaptee>();
+            var adapter = new Adapter(adaptee.Object);
+
+            adapter.BeautifulMethod();
+
+            adaptee.Verify(i => i.Authenticate());
+            adaptee.Verify(i => i.FarTooLongSignatureOfMethodWhichShouldGetWrapped());
+        }
+    }
+}
