0e174b809998e6e5d9fb7e6f95efcb70ce529a0b
Marius Wichtner <mwichtner@coconut-capital.com>
2018-02-27 00:27:35 +0100

    Added Proxy Pattern

---
+++ DesignPatterns/Proxy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DesignPatterns
+{
+    interface IApi
+    {
+        string DoSth();
+    }
+
+    public class Api : IApi
+    {
+        public string DoSth() => "doing sth";
+    }
+
+    public class ApiProxy : IApi
+    {
+        public string DoSth() => "proxy";
+    }
+}
---
+++ DesignPatternsTest/ProxyTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.InteropServices.ComTypes;
+using DesignPatterns;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace DesignPatternsTest
+{
+    public class ProxyTests
+    {
+        [Fact]
+        public void ProxyTest()
+        {
+            var proxy = new ApiProxy();
+
+            proxy.DoSth().Should().Be("proxy");
+        }
+    }
+}
