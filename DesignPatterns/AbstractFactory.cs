878a6fbc28de9f2aaa1d2f08d034cae71004529e
Marius Wichtner <mwichtner@coconut-capital.com>
2018-02-27 00:47:42 +0100

    Added Abstract Factory

---
+++ DesignPatterns/AbstractFactory.cs
@@ -0,0 +1,32 @@
+﻿namespace DesignPatterns
+{
+    public interface IUiElement
+    {
+    }
+
+    public class Button : IUiElement
+    {
+    }
+
+    public class Window : IUiElement
+    {
+    }
+
+    /**
+     * Anti-Pattern -> Use dependency injection instead if possible
+     */
+    public interface IUiElementFactory
+    {
+        IUiElement Create();
+    }
+
+    public class ButtonFactory : IUiElementFactory
+    {
+        public IUiElement Create() => new Button();
+    }
+
+    public class WindowFactory : IUiElementFactory
+    {
+        public IUiElement Create() => new Window();
+    }
+}
---
+++ DesignPatternsTest/AbstractFactoryTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.InteropServices.ComTypes;
+using DesignPatterns;
+using Moq;
+using Xunit;
+
+namespace DesignPatternsTest
+{
+    public class AbstractFactoryTest
+    {
+        [Fact]
+        public void CommandPatternTest()
+        {
+            var button = new ButtonFactory().Create();
+            Assert.IsType<Button>(button);
+
+            var window = new WindowFactory().Create();
+            Assert.IsType<Window>(window);
+        }
+    }
+}
